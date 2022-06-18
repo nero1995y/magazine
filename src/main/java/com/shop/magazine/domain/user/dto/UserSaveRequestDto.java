a66d4482a474adcfd719dc3d0f4abe5d6357329e
nero <wnsgur765z@naver.com>
2022-06-18 22:59:28 +0900

    feat; Add user save DTO

---
+++ src/main/java/com/shop/magazine/domain/user/dto/UserSaveRequestDto.java
@@ -0,0 +1,33 @@
+package com.shop.magazine.domain.user.dto;
+
+import com.shop.magazine.domain.user.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserSaveRequestDto {
+    private String email;
+    private String name;
+    private String password;
+    private String phone;
+
+    @Builder
+    public UserSaveRequestDto(String email, String name, String password, String phone) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public User toEntity() {
+        return User.builder()
+                .email(this.email)
+                .name(this.name)
+                .password(this.password)
+                .phone(this.phone)
+                .build();
+
+    }
+}
