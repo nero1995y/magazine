6de1fb525a574838fe146bf4c692be98dffd2210
nero <wnsgur765z@naver.com>
2022-06-18 23:03:11 +0900

    feat: Add user email repository 메소드

--- src/main/java/com/shop/magazine/domain/user/repository/UserRepository.java
+++ src/main/java/com/shop/magazine/domain/user/repository/UserRepository.java
@@ -2,6 +2,10 @@ package com.shop.magazine.domain.user.repository;
 
 import com.shop.magazine.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(@Param("email") String email);
+
 }
