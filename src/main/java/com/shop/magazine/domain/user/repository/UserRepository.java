791704b5eb28ef63d10b61953f6985d5f69ad869
nero <wnsgur765z@naver.com>
2022-06-26 14:49:55 +0900

    feat: Add Email 중복 메소드

--- src/main/java/com/shop/magazine/domain/user/repository/UserRepository.java
+++ src/main/java/com/shop/magazine/domain/user/repository/UserRepository.java
@@ -2,10 +2,11 @@ package com.shop.magazine.domain.user.repository;
 
 import com.shop.magazine.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(@Param("email") String email);
+    Optional<User> findByEmail(@Param("email") String email);
 
 }
