efa6666e753bf02c5d5adf0eabef382232c6e37c
nero <wnsgur765z@naver.com>
2022-06-26 14:47:30 +0900

    feat: Add user service
    crud . 이메일 중복 메소드

--- src/main/java/com/shop/magazine/domain/user/service/UserService.java
+++ src/main/java/com/shop/magazine/domain/user/service/UserService.java
@@ -1,7 +1,12 @@
 package com.shop.magazine.domain.user.service;
 
+import com.shop.magazine.domain.user.dto.UserResponseDto;
 import com.shop.magazine.domain.user.dto.UserSaveRequestDto;
+import com.shop.magazine.domain.user.dto.UserUpdateRequestDto;
+import com.shop.magazine.domain.user.dto.UsersResponseDto;
 import com.shop.magazine.domain.user.entity.User;
+import com.shop.magazine.domain.user.exception.AlreadyUserException;
+import com.shop.magazine.domain.user.exception.UserNotFoundException;
 import com.shop.magazine.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,10 +22,47 @@ public class UserService {
     @Transactional
     public Long register(UserSaveRequestDto requestDto) {
 
+        verifyEmail(requestDto.getEmail());
+
         return userRepository
                 .save(requestDto.toEntity())
                 .getId();
     }
+
+    private void verifyEmail(String email) {
+        userRepository
+                .findByEmail(email)
+                .ifPresent((user) -> {
+                    throw new AlreadyUserException();
+                });
+    }
+    // TODO (nero) exception 테스트 만들기
+    public UserResponseDto findById(Long id) {
+        return new UserResponseDto(findUser(id));
+    }
+
+    public UsersResponseDto findAll() {
+        return  UsersResponseDto.of(userRepository.findAll());
+    }
+
+    @Transactional
+    public void update(UserUpdateRequestDto requestDto) {
+        User user = findUser(requestDto.toEntity().getId());
+        user.update(requestDto.toEntity());
+    }
+
+    @Transactional
+    public void remove(Long id) {
+        User user = findUser(id);
+        userRepository.deleteById(user.getId());
+    }
+
+
+    private User findUser(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(UserNotFoundException::new);
+    }
+
 }
 
 
