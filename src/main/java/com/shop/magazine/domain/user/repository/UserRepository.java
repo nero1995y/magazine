37fc5a723f68d65ed8461e1eb2a8d9e17adcf820
nero <wnsgur765z@naver.com>
2022-07-10 22:16:04 +0900

    feat: Add user repository 조회 메소드 추가

--- src/main/java/com/shop/magazine/domain/user/repository/UserRepository.java
+++ src/main/java/com/shop/magazine/domain/user/repository/UserRepository.java
@@ -1,12 +1,23 @@
 package com.shop.magazine.domain.user.repository;
 
 import com.shop.magazine.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
     Optional<User> findByEmail(@Param("email") String email);
+
+    @Query("select u from User u where u.username in :usernames")
+    List<User> findByUsernames(@Param("usernames") List<String> usernames);
+
+    @Query("select u from User u order by u.username asc")
+    Page<User> findAllOrderByUsernameAsc(Pageable pageable);
+
 
 }
