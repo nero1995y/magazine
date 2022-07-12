74962149629e8795c14a6727a04efc49d31a7311
nero <wnsgur765z@naver.com>
2022-07-12 21:26:03 +0900

    feat: Add auditing entity

---
+++ src/main/java/com/shop/magazine/global/entity/AuditingEntity.java
@@ -0,0 +1,24 @@
+package com.shop.magazine.global.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class AuditingEntity {
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
