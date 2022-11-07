0433cdfed0f2a8aa43c61ed54567810a540e9576
nero <wnsgur765z@naver.com>
2022-11-07 22:25:46 +0900

    feat: Add security config

---
+++ src/main/java/com/shop/magazine/global/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.shop.magazine.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll()
+                .and()
+
+                // 로그인 페이지로 이동
+                .formLogin()
+                .loginPage("/login");
+
+        return http.build();
+    }
+}
