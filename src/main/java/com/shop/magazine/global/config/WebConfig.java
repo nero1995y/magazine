abcd7cbaa4d752e80b78879f5dc35c7f546ce451
nero <wnsgur765z@naver.com>
2022-11-09 21:19:52 +0900

    style: Change web config 메소드 공백
    addCorsMappings 메소드 수정

--- src/main/java/com/shop/magazine/global/config/WebConfig.java
+++ src/main/java/com/shop/magazine/global/config/WebConfig.java
@@ -1,14 +1,16 @@
 package com.shop.magazine.global.config;
 
+import org.springframework.boot.web.servlet.view.MustacheViewResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public  void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000");
 
