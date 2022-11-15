632b3ac1d5ef9d70a6c8a243487da250571be4ea
nero <wnsgur765z@naver.com>
2022-11-15 22:57:37 +0900

    remove: category repository

--- src/main/java/com/shop/magazine/domain/post/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shop.magazine.domain.post.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-
-}
